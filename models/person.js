00faac63594bc24b56131a23152d6ece1583bda3
Sami Horttanainen <horttsam@ad.helsinki.fi>
2021-04-23 14:32:39 +0300

    tietokantaa käyttävä versio internettiin

---
+++ models/person.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const url = process.env.MONGODB_URI
+
+console.log('connection to', url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+.then(result => {
+  console.log('connected to MongoDB')
+})
+.catch((error) => {
+  console.log('error conecting to MongoDB')
+})
+
+const personSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    unique: true
+  },
+  number: {
+    type: String,
+    minLength : 8,
+    required: true
+  }
+})
+personSchema.plugin(uniqueValidator)
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject.__v
+    delete returnedObject._id
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
